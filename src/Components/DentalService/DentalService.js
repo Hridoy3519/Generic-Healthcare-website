92904daddee16176bc025311147003f9aa6c0a02
Hridoy Chowdhury <hridoy3519@gmail.com>
2021-10-18 13:54:40 +0600

    Services added

---
+++ src/Components/DentalService/DentalService.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Button, Card, Col } from "react-bootstrap";
+import './DentalServices.css';
+const DentalService = (props) => {
+  const { img, title, description } = props.service;
+  return (
+    <Col>
+      <Card className="h-100 card">
+        <Card.Img variant="top" src={img} />
+        <Card.Body className="card-body">
+          <Card.Title> {title}</Card.Title>
+          <Card.Text>
+            {description}
+          </Card.Text>
+        </Card.Body>
+        <Card.Footer className="card-footer text-center">
+            <Button>See Details</Button>
+        </Card.Footer>
+      </Card>
+    </Col>
+  );
+};
+
+export default DentalService;
---
+++ src/Components/Services/Services.js
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from "react";
+import { Container, Row } from "react-bootstrap";
+import DentalService from "../DentalService/DentalService";
+import "./Services.css";
+const Services = () => {
+  const [services, setServices] = useState([]);
+
+  useEffect(() => {
+    fetch("./services.json")
+      .then((res) => res.json())
+      .then((data) => setServices(data));
+  });
+  return (
+    <Container className="services">
+      <h5 className="colored-text text-center mt-5">
+        Our Best Dental Services
+      </h5>
+      <h1>Enjoy Specialized Care Through</h1>
+      <h1>Precision, Artistry, and Experience</h1>
+
+      <Row xs={1} md={3} className="g-4 mt-2">
+        {
+            services.map(service => <DentalService service={service}></DentalService> )
+        }
+      </Row>
+      
+    </Container>
+  );
+};
+
+export default Services;
